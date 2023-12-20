7b1e2718bb6e1ae77d8c396e6821de709a763d60
sujith <sjklearning@gmail.com>
2023-12-20 16:10:29 +0530

    Add project files.

---
+++ ToDo-web application/Controllers/TodoController.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ToDo_web_application.Data;
+using ToDo_web_application.Models;
+
+[Route("api/[controller]")]
+[ApiController]
+public class TodoController : ControllerBase
+{
+    private readonly TodoDbContext _context;
+
+    public TodoController(TodoDbContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<TodoItem>>> GetTodoItems()
+    {
+        return await _context.TodoItems.ToListAsync();
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<TodoItem>> GetTodoItem(int id)
+    {
+        var todoItem = await _context.TodoItems.FindAsync(id);
+
+        if (todoItem == null)
+        {
+            return NotFound();
+        }
+
+        return todoItem;
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<TodoItem>> CreateTodoItem(TodoItem item)
+    {
+        _context.TodoItems.Add(item);
+        await _context.SaveChangesAsync();
+
+        return CreatedAtAction(nameof(GetTodoItem), new { id = item.Id }, item);
+    }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> EditTodoItem(int id, TodoItem item)
+    {
+        if (id != item.Id)
+        {
+            return BadRequest();
+        }
+
+        _context.Entry(item).State = EntityState.Modified;
+        await _context.SaveChangesAsync();
+
+        return NoContent();
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteTodoItem(int id)
+    {
+        var todoItem = await _context.TodoItems.FindAsync(id);
+
+        if (todoItem == null)
+        {
+            return NotFound();
+        }
+
+        _context.TodoItems.Remove(todoItem);
+        await _context.SaveChangesAsync();
+
+        return NoContent();
+    }
+}
---
+++ ToDo-web application/Data/TodoDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ToDo_web_application.Models;
+
+namespace ToDo_web_application.Data
+{
+    public class TodoDbContext : DbContext
+    {
+        public TodoDbContext(DbContextOptions<TodoDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<TodoItem> TodoItems { get; set; }
+    }
+}
